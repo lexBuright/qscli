763f1349e39f8bd9cadb54190316ef756467aeaa
Lex Buright <lex.buright@gmail.com>
2016-08-31 19:01:39 -0500

    symbol -- Equivalent of erlang atoms

---
+++ qscli/symbol.py
@@ -0,0 +1,9 @@
+class Symbol(object):
+    def __init__(self, name):
+        self._name = name
+
+    def __eq__(self, other):
+        if isinstance(other, Symbol):
+            return self._name == other._name
+        else:
+            return False
