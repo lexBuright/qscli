4d916260dd773ec9e498f4dc35f459233542cd5e
Lex Buright <lex.buright@gmail.com>
2016-09-06 23:12:29 -0500

    symbols -- better reprs

--- qscli/symbol.py
+++ qscli/symbol.py
@@ -2,6 +2,9 @@ class Symbol(object):
     def __init__(self, name):
         self._name = name
 
+    def __repr__(self):
+        return 'Symbol({!r})'.format(self._name)
+
     def __eq__(self, other):
         if isinstance(other, Symbol):
             return self._name == other._name
