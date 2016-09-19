8b5f222641ff3d3163d9288a98166ce71e77567a
Lex Buright <lex.buright@gmail.com>
2016-09-19 09:35:55 -0500

    qstimeseries -- various
    * Store entries with ids and look up by id
    * Delete by id

--- qscli/qstimeseries.py
+++ qscli/qstimeseries.py
@@ -9,9 +9,11 @@ qstimeseries append stringseries --string "this is a string value"
 
 import argparse
 import datetime
+import json
 import logging
 import os
 import sqlite3
+import time
 
 LOGGER = logging.getLogger()
 
@@ -25,7 +27,11 @@ def ensure_database(config_dir):
             db = sqlite3.connect(data_file)
             cursor = db.cursor()
             cursor.execute('''
-            CREATE TABLE timeseries(id INTEGER PRIMARY KEY, time TIMESTAMP DEFAULT CURRENT_TIMESTAMP, series TEXT, float_value REAL, string_value TEXT);
+            CREATE TABLE timeseries(id INTEGER PRIMARY KEY, series TEXT NOT NULL, given_ident TEXT, time TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL, float_value REAL, string_value TEXT,
+
+            CONSTRAINT unique_ident UNIQUE (series, given_ident)
+
+            );
             ''')
             db.commit()
             return db
@@ -37,29 +43,48 @@ def ensure_database(config_dir):
 
     return sqlite3.connect(data_file)
 
-def append(db, series, value_string, value_type):
+def append(db, series, value_string, value_type, ident):
     value = value_type(value_string)
     cursor = db.cursor()
     value_field = {str: 'string_value', float: 'float_value'}[value_type]
-    cursor.execute('INSERT INTO timeseries(series, {}) values (?, ?)'.format(value_field), (series, value))
+    cursor.execute('INSERT INTO timeseries(series, {}, given_ident) values (?, ?, ?)'.format(value_field), (series, value, ident))
     db.commit()
 
-def show(db, series):
+def show(db, series, ident, json_output):
     cursor = db.cursor()
 
-    query = ['SELECT time, series, coalesce(float_value, string_value) from timeseries']
     query_options = []
-
+    wheres = []
     if series:
-        query.append('where series=?')
+        wheres.append('series=?')
         query_options.append(series)
 
-    query.append('order by time')
+    if ident:
+        wheres.append('given_ident=?')
+        query_options.append(ident)
 
-    cursor.execute(' '.join(query), query_options)
-    for time_string, series, value in  cursor.fetchall():
-        time = datetime.datetime.strptime(time_string, '%Y-%m-%d %H:%M:%S')
-        print time.isoformat(), series, value
+    query1 = 'SELECT time, series, given_ident, coalesce(float_value, string_value) from timeseries'
+    query2 = 'order by time'
+
+    if wheres:
+        query = '{} where {} {}'.format(query1, 'AND '.join(wheres), query2)
+    else:
+        query = '{} {}'.format(query1, query2)
+
+    cursor.execute(query, query_options)
+    if not json_output:
+        result = []
+        for time_string, series, ident, value in cursor.fetchall():
+            dt = datetime.datetime.strptime(time_string, '%Y-%m-%d %H:%M:%S')
+            result.append('{} {} {} {}'.format(dt.isoformat(), ident, series, value))
+        return '\n'.join(result)
+    else:
+        result = []
+        for time_string, series, ident, value in cursor.fetchall():
+            dt = datetime.datetime.strptime(time_string, '%Y-%m-%d %H:%M:%S')
+            unix_time = time.mktime(dt.timetuple())
+            result.append(dict(time=unix_time, series=series, id=ident, value=value))
+        return json.dumps(result)
 
 
 PARSER = argparse.ArgumentParser(description='Very simple command line timeseries')
@@ -71,10 +96,18 @@ parsers = PARSER.add_subparsers(dest='command')
 append_command = parsers.add_parser('append', help='Add a value')
 append_command.add_argument('series', type=str, help='Timeseries')
 append_command.add_argument('--string', action='store_const', dest='value_type', const=str, default=float)
+append_command.add_argument('--id', type=str, help='Name a value', dest='ident')
 append_command.add_argument('value', type=str)
 
 show_command = parsers.add_parser('show', help='Show the values in a series')
-show_command.add_argument('--series', type=str, help='Timeseries')
+show_command.add_argument('--series', type=str, help='Only show this timeseries')
+show_command.add_argument('--id', type=str, help='Only show the entry with this id', dest='ident')
+show_command.add_argument('--json', action='store_true', help='Output in machine readable json')
+
+delete_parser = parsers.add_parser('delete', help='Delete a value from a timeseries')
+delete_parser.add_argument('series', type=str, help='Which series to delete from')
+mx = delete_parser.add_mutually_exclusive_group(required=True)
+mx.add_argument('--id', type=str, help='Delete entry with this id', dest='ident')
 
 
 def main():
@@ -90,11 +123,21 @@ def main():
 
     db = ensure_database(config_dir)
     if options.command == 'append':
-        append(db, options.series, options.value, options.value_type)
+        append(db, options.series, options.value, options.value_type, options.ident)
     elif options.command == 'show':
-        show(db, options.series)
+        print show(db, options.series, options.ident, options.json)
+    elif options.command == 'delete':
+        delete(db, options.series, options.ident)
     else:
         raise ValueError(options.command)
+
+def delete(db, series, ident):
+    cursor = db.cursor()
+    cursor.execute('''
+    DELETE FROM timeseries
+    WHERE series=? AND given_ident=?
+    ''', (series, ident))
+    db.commit()
 
 
 if __name__ == '__main__':
