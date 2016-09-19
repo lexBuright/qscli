2bec18679e8e3de36fa40f865e7fbf8e5e18502b
Lex Buright <lex.buright@gmail.com>
2016-09-19 09:30:59 -0500

    qsrct -- More of the initial implementation
    * Allow specifing of an external data source for testing
    * testing with bonferroni detection
    * data command to display collected data
    * timeseries command to interact with underlying store

--- qscli/qsrct.py
+++ qscli/qsrct.py
@@ -23,16 +23,19 @@ qsrct test jumping --data <(qstimeseries show jumping) --method alternative # us
 Bon ferroni is overly aggressive, there are alternative approaches for
 calculating the significance loss for repeatedly looking at results.
 
-This tool represents represents a convenience wrapper, you might be
-better off using R for certain things.
+This tool represents represents a convenience wrapper, you might be better coding your own tests in R or python.
 
 Lots of things can break independence assumptions when experimenting on yourself...
 """
 
 import argparse
+import collections
 import contextlib
 import csv
+import datetime
 import json
+import logging
+import math
 import os
 import random
 import shutil
@@ -41,17 +44,19 @@ import subprocess
 import sys
 import tempfile
 import threading
+import time
 import unittest
 
 import fasteners
 
+LOGGER = logging.getLogger()
 
 def parse_csv_line(line):
     result, = tuple(csv.reader(StringIO.StringIO(line)))
     return result
 
 def run(qsrct, argv):
-    options = PARSER.parse_args(argv)
+    options = build_parser().parse_args(argv)
     return qsrct.run(options)
 
 class FakeRandom(object):
@@ -69,17 +74,22 @@ class FakeRandom(object):
         return self.destined_values.pop(0)
 
 def main():
+    if '--debug' in sys.argv:
+        logging.basicConfig(level=logging.DEBUG)
+
     if '--test' in sys.argv:
         sys.argv.remove('--test')
         unittest.main()
     else:
         # Double-parse to get config_dir
         #    while using run for testing
-        options = PARSER.parse_args()
+        options = build_parser().parse_args()
         result = (run(QsRct(random, options.config_dir), sys.argv[1:]))
         if result:
-            sys.stdout.write(result)
-            sys.stdout.flush()
+            if result:
+                sys.stdout.write(result)
+                sys.stdout.write('\n')
+                sys.stdout.flush()
 
 class QsRct(object):
     def __init__(self, random, config_dir):
@@ -98,10 +108,8 @@ class QsRct(object):
     def run(self, options):
         self.ensure_config()
 
-        if options.command == 'new':
-            return self.new(options.name, options.options or [])
-        elif options.command == 'edit':
-            return self.edit(options.name, description=options.description)
+        if options.command == 'edit' or options.command == 'new':
+            return self.edit(options.name, description=options.description, options=options.options, assign_period=options.assign_period, data_source=options.data_source)
         elif options.command == 'assign':
             return self.assign(options.name)
         elif options.command == 'assignments':
@@ -113,47 +121,173 @@ class QsRct(object):
             return self.delete(options.name)
         elif options.command == 'show':
             return self.show(options.name)
+        elif options.command == 'test':
+            return self.test(options.name)
+        elif options.command == 'data':
+            return self.data(options.name)
+        elif options.command == 'timeseries':
+            return self.timeseries(options.timeseries_command)
         else:
             raise ValueError(options.command)
+
+    def timeseries(self, command):
+        return self._timeseries_run(command)
 
     def assign(self, name):
         with self._with_experiment_data(name) as experiment_data:
             options = experiment_data['options']
+            if not options:
+                raise Exception('No options to choose from')
             assignment = self._random.choice(list(options))
-            self._timeseries_run(['append', 'assignments.{}'.format(name), '--string', assignment])
+
+            if experiment_data['assign_period'] is not None:
+                ident = datetime.datetime.utcfromtimestamp(int(time.time()) // experiment_data['assign_period'] * experiment_data['assign_period']).isoformat()
+            else:
+                ident = None
+
+            if ident:
+                result = json.loads(self._timeseries_run([
+                    'show',
+                    '--series', 'assignments.{}'.format(name),
+                    '--id', str(ident),
+                    '--json'
+                ]))
+
+                if result:
+                    LOGGER.debug('Reusing assignment')
+                    return result[0]['value']
+
+            LOGGER.debug('Creating a new assignment')
+            command = ['append', 'assignments.{}'.format(name), '--string', assignment]
+            if ident:
+                command += ['--id', str(ident)]
+
+            self._timeseries_run(command)
             return assignment
 
     def assignments(self, name):
-        return self._timeseries_run(['show', 'assignments.{}'.format(name)])
+        return self._timeseries_run(['show', '--series', 'assignments.{}'.format(name)])
 
     @contextlib.contextmanager
-    def _with_experiment_data(self, name):
+    def _with_experiment_data(self, name, create=True):
         with with_data(self._data_file) as data:
             experiments = data.setdefault('experiments', dict())
-            yield experiments.setdefault(name, dict())
+            if create:
+                yield experiments.setdefault(name, dict())
+            else:
+                yield experiments[name]
 
-    def new(self, name, options):
+    def edit(self, name, description, options, assign_period, data_source):
         with self._with_experiment_data(name) as experiment_data:
-            experiment_data['options'] = options
+            if 'description' not in experiment_data:
+                experiment_data['description'] = ''
+            if 'options' not in experiment_data:
+                experiment_data['options'] = []
+            if 'assign_period' not in experiment_data:
+                experiment_data['assign_period'] = None
+            if 'data_source' not in experiment_data:
+                experiment_data['data_source'] = None
 
-    def edit(self, name, description):
-        with self._with_experiment_data(name) as experiment_data:
+            if data_source is not None:
+                experiment_data['data_source'] = data_source
             if description is not None:
                 experiment_data['description'] = description
+            if options is not None:
+                experiment_data['options'] = options
+            if assign_period is not None:
+                experiment_data['assign_period'] = assign_period
 
     def trials(self):
         with with_data(self._data_file) as data:
             return '\n'.join(data['experiments'])
 
+    def test(self, name):
+        LOGGER.debug('Testing')
+        from scipy import stats
+        with self._with_experiment_data(name) as experiment_data:
+            options = experiment_data['options']
+
+        if len(options) == 2:
+            return self._ttest(experiment_data, name)
+        else:
+            raise ValueError(options)
+
+    def _ttest(self, experiment_data, name):
+        from scipy import stats
+        LOGGER.debug('Running t-test')
+        option1, option2 = experiment_data['options']
+
+        num_tests = experiment_data.setdefault('num_tests', 1)
+
+        data_points = self.get_data(name)
+        sample1 = [x.value for x in data_points if x.assignment == option1]
+        sample2 = [x.value for x in data_points if x.assignment == option2]
+        result = stats.ttest_ind(sample1, sample2, equal_var=True)
+        if math.isnan(result.pvalue):
+            return 'Not enough data for test'
+        else:
+            # Work out the exact p-value rather
+            #   than use approximation (more accurate)
+
+            corrected_pvalue = (1 - (1 - result.pvalue)) ** (float(1) / num_tests)
+
+            experiment_data.setdefault('num_tests', num_tests + 1)
+
+            interval = stats.t.interval()
+
+            result = []
+            result.append('Corrected p-value: {}'.format(corrected_pvalue))
+            result.append('Number of tests: {}'.format(num_tests))
+            result.append('Cheat p-value: {}'.format(result.pvalue))
+
+
+    def get_data(self, name):
+        with self._with_experiment_data(name) as experiment_data:
+            if experiment_data['data_source']:
+                command = str(experiment_data['data_source']).split(' ')
+                LOGGER.debug('command %r', command)
+                raw_result = subprocess.check_output(command)
+                results = list(csv.reader(raw_result.splitlines()))
+            else:
+                raise NotImplementedError('Getting results')
+
+            assignments = self.get_assignments(name)
+            data_points = []
+            data_idents = set()
+            for ident, value in results:
+                if ident in assignments:
+                    data_points.append(DataPoint(ident=ident, value=float(value), assignment=assignments[ident]))
+                    data_idents.add(ident)
+
+            for ident in assignments:
+                if ident not in data_idents:
+                    data_points.append(DataPoint(ident=ident, value=None, assignment=assignments[ident]))
+
+            return data_points
+
+    def get_assignments(self, name):
+        data = json.loads(self._timeseries_run(['show', '--series', 'assignments.{}'.format(name), '--json']))
+        result = {}
+        for assignment in data:
+            result[assignment['id']] = assignment['value']
+
+        return result
+
     def delete(self, name):
         with with_data(self._data_file) as data:
             del data['experiments'][name]
+
+    def data(self, name):
+        data_points = self.get_data(name)
+        for point in data_points:
+            print point.ident, point.assignment, point.value
 
     def show(self, name):
         result = []
         with self._with_experiment_data(name) as experiment_data:
             result.append('options: ' + ' '.join(sorted(experiment_data['options'])))
             result.append('description: ' + experiment_data['description'])
+            result.append('data_source:' + experiment_data['data_source'])
 
         return '\n'.join(result)
 
@@ -163,6 +297,7 @@ class QsRct(object):
             '--config-dir',
             self._timeseries_data_dir] + command)
 
+DataPoint = collections.namedtuple('DataPoint', 'ident value assignment')
 
 
 def backticks(command, stdin=None, shell=False):
@@ -253,7 +388,6 @@ class TestQsrct(unittest.TestCase):
         result = self.run_cli(['show', 'test'])
         self.assertTrue('description: some testing' in result, result)
 
-
     def test_basic(self):
         self.run_cli(['new', 'test', '--options', 'good,bad'])
 
@@ -277,18 +411,25 @@ class TestQsrct(unittest.TestCase):
 
 
 def build_parser():
-    PARSER = argparse.ArgumentParser(description='Convenience tool to run randomized controlled trial ')
-    PARSER.add_argument('--config-dir', type=str, help='Where to store configuration', default=DEFAULT_DATA_DIR)
-    PARSER.add_argument('--debug', action='store_true', help='Include debug output')
-    parsers = PARSER.add_subparsers(dest='command')
+    parser = argparse.ArgumentParser(description='Convenience tool to run randomized controlled trial ')
+    parser.add_argument('--config-dir', type=str, help='Where to store configuration', default=DEFAULT_DATA_DIR)
+    parser.add_argument('--debug', action='store_true', help='Include debug output')
+    parsers = parser.add_subparsers(dest='command')
 
-    new = parsers.add_parser('new', help='Create a new experiment')
-    new.add_argument('name', type=str)
-    new.add_argument('--options', type=parse_csv_line, help='Possible values for the experiment')
+    def new_or_edit(parsers, name):
+        new = parsers.add_parser(name, help='Create a new experiment')
+        new.add_argument('name', type=str)
+        new.add_argument('--options', type=parse_csv_line, help='Possible values for the experiment')
+        new.add_argument('--description', type=str, help='Description of the experiment')
+        new.add_argument('--every', type=parse_period, help='Only assign every period (e.g. 1d, 2h)', dest='assign_period')
+        new.add_argument('--no-period', action='store_const', const=None, dest='assign_period', help='Give a new assignment every time assign is called')
+        new.add_argument('--data-source', type=str, help='Command run to fetch data')
 
-    edit = parsers.add_parser('edit', help='Edit an existing experiment')
-    edit.add_argument('name', type=str)
-    edit.add_argument('--description', type=str, help='Information about the experiment')
+    new_or_edit(parsers, 'new')
+    new_or_edit(parsers, 'edit')
+
+    timeseries = parsers.add_parser('timeseries', help='Interact with underlying timeseries (see qstimeseries). LIABLE TO CHANGE')
+    timeseries.add_argument('timeseries_command', nargs='+')
 
     assign = parsers.add_parser('assign', help='Randomly assign a subject to a particular experiment')
     assign.add_argument('name', type=str)
@@ -296,6 +437,9 @@ def build_parser():
     result = parsers.add_parser('result', help='Record a result for this assignment (not necessary if using another source for storage)')
     result.add_argument('name', type=str)
     result.add_argument('value', type=float, help='Value of experiment')
+
+    data = parsers.add_parser('data', help='Display the data collected for an experiment')
+    data.add_argument('name', type=str)
 
     assignment = parsers.add_parser('assignments', help='Show the assignments for an experiment')
     assignment.add_argument('name', type=str)
@@ -309,10 +453,20 @@ def build_parser():
     show = parsers.add_parser('show', help='Show information about an experiment')
     show.add_argument('name', type=str)
 
-    test = parsers.add_parser('trials', help='List all trials')
-    return PARSER
+    trials = parsers.add_parser('trials', help='List all trials')
+    return parser
 
-PARSER = build_parser()
+UNITS = {
+    's': 1,
+    'd': 86400,
+    'm': 60,
+    'h': 3600,
+             }
+
+def parse_period(string):
+    unit = string[-1]
+    number = int(string[:-1])
+    return number * UNITS[unit]
 
 
 
