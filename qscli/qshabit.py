99da2ecb3d83cc0d086e69ffdb4845ba2a26809a
Lex Buright <lex.buright@gmail.com>
2016-10-27 05:06:15 -0500

    qshabit -- first cut of habit tracking

---
+++ qscli/qshabit.py
@@ -0,0 +1,6 @@
+"""
+Command line habit forming tool which degrades well. Periodically remind you about things you want to do, prioritizing the most important activities.
+
+Lots of choices: regularity period. I think we just throw some mud at a wall and play
+
+"""
