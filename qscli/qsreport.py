e71a4b3ae06f9c5357e31f70e95d90e3b209f8a0
Lex Buright <lex.buright@gmail.com>
2016-12-09 10:12:18 +0000

    qsreport -- support --config-dir

--- qscli/qsreport.py
+++ qscli/qsreport.py
@@ -44,11 +44,11 @@ LOGGER = logging.getLogger()
 #    - Part of the purpose of this tool is for interaction with guis. I want to bind given commands to
 #    various keybindings. It seems appropriate to put the window-manager agnostic code into
 #    a single place. As to why one needs to be able to do this sort of thing from a window manager,
-#    this mostly comes down to making easy things easy again: you want to be able to quickly
+#    this mostly comes down to making easy things easy once again: you want to be able to quickly
 #    look at various reports without interrupting your other activities
 
 #    - Reuseability! This script represents a way of doing things; by creating it, I create a way
-#    of spreading these ideas
+#    of spreading these ideas.
 
 #    - A combined interface for use and editing. There is a frictional cost to changing things when you
 #    notice they could be improved. You have to stop what you are doing, open a new window, or
@@ -58,15 +58,7 @@ LOGGER = logging.getLogger()
 #    goes unlooked at.
 
 
-DATA_DIR = os.path.join(os.environ['HOME'], '.config', 'qsreport')
-if not os.path.isdir(DATA_DIR):
-   os.mkdir(DATA_DIR)
-
-REPORT_DIR = os.path.join(DATA_DIR, 'reports')
-if not os.path.isdir(DATA_DIR):
-   os.mkdir(REPORT_DIR)
-
-DATA_FILE = os.path.join(DATA_DIR, 'data.json')
+DEFAULT_CONFIG_DIR = os.path.join(os.environ['HOME'], '.config', 'qsreport')
 
 
 LAST = Symbol('last')
@@ -74,6 +66,7 @@ PROMPT = Symbol('prompt')
 
 PARSER = argparse.ArgumentParser(description=__doc__)
 PARSER.add_argument('--debug', action='store_true', help='Print debug output')
+PARSER.add_argument('--config-dir', '-C', type=str, help='Use this directory of configuration', default=DEFAULT_CONFIG_DIR)
 parsers = PARSER.add_subparsers(dest='command')
 
 path_command = parsers.add_parser('path', help='Show the path of a report')
@@ -94,7 +87,6 @@ show_command.add_argument('--prompt-for-name', action='store_true', help='Prompt
 delete_command = parsers.add_parser('delete', help='Show a new report')
 delete_command.add_argument('name', type=str)
 
-
 random_command = parsers.add_parser('random', help='Show a new report')
 
 def main():
@@ -102,70 +94,85 @@ def main():
     if args.debug:
         logging.basicConfig(level=logging.DEBUG)
 
+    data = Data(args.config_dir)
+
     if args.command in ('new', 'edit'):
-        new(args.name, args.editor)
+        new(data, args.name, args.editor)
     elif args.command == 'show':
-        show(args.name, args.prompt_for_name)
+        show(data, args.name, args.prompt_for_name)
     elif args.command == 'path':
-        path(args.name)
+        path(data, args.name)
     elif args.command == 'blah':
         raise NotImplementedError()
     elif args.command == 'random':
-        show(random.choice(get_reports()), False)
+        show(data, random.choice(data.get_reports()), False)
     elif args.command == 'delete':
-        delete(args.name)
+        delete(data, args.name)
     else:
         raise ValueError(args.command)
-
-def get_reports():
-    return [x for x in os.listdir(REPORT_DIR) if not x.endswith('~')]
 
 def get_default_editors():
     return [os.environ.get('VISUAL'), os.environ.get('EDITOR'), 'sensible-editor', 'vim', 'vi', 'nano', 'ed']
 
-def new(name, editor):
+def new(data, name, editor):
     if name == LAST:
-        name = Data.get_last()
+        name = data.get_last()
     elif name is None:
-        name = guiutils.combo_prompt('Report name', get_reports())
+        name = guiutils.combo_prompt('Report name', data.get_reports())
 
     editors = [editor] if editor else None
-    report_path = os.path.join(REPORT_DIR, name)
+    report_path = data.get_report_path(name)
     edit.edit(report_path, editors=editors)
     os.chmod(report_path, os.stat(report_path).st_mode | stat.S_IEXEC)
 
-def delete(name):
-    report_path = os.path.join(REPORT_DIR, name)
-    os.unlink(report_path)
+def delete(data, name):
+    os.unlink(data.get_report_path(name))
 
-def show(name, prompt_for_name):
+def show(data, name, prompt_for_name):
     if name is not None and prompt_for_name:
         raise Exception('You must either use --prompt-for-name or specify a report, not both')
 
     if prompt_for_name:
-        name = guiutils.combo_prompt('Report name', get_reports())
-    LOGGER.debug('Showing %s', name)
-    Data.set_last(name)
-    print name
-    report_path = os.path.join(REPORT_DIR, name)
-    subprocess.check_call([report_path])
+        name = guiutils.combo_prompt('Report name', data.get_reports())
 
-def path(name):
+    LOGGER.debug('Showing %s', name)
+    data.set_last(name)
+    print name
+    subprocess.check_call([data.get_report_path(name)])
+
+def path(data, name):
     if name == LAST:
-        name = Data.get_last()
-    print os.path.join(REPORT_DIR, name)
+        name = data.get_last()
+    return data.get_report_path(name)
 
 class Data(object):
-    @staticmethod
-    def get_last():
-        with with_data(DATA_FILE) as data:
+    def __init__(self, config_dir):
+        self._config_dir = config_dir
+        self._data_file = os.path.join(config_dir, 'data.json')
+        self._report_dir = os.path.join(config_dir, 'reports')
+
+    def _init_config(self):
+        if not os.path.isdir(self._config_dir):
+            os.mkdir(self._config_dir)
+
+        if not os.path.isdir(self._report_dir):
+            os.mkdir(self._report_dir)
+
+    def get_last(self):
+        self._init_config()
+        with with_data(self._data_file) as data:
             return data['last']
 
-    @staticmethod
-    def set_last(last):
-        with with_data(DATA_FILE) as data:
+    def set_last(self, last):
+        self._init_config()
+        with with_data(self._data_file) as data:
             data['last'] = last
 
+    def get_reports(self):
+        return [x for x in os.listdir(self._report_dir) if not x.endswith('~')]
+
+    def get_report_path(self, name):
+        return os.path.join(self._report_dir, name)
 
 if __name__ == '__main__':
     main()
