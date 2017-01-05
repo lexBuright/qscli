9f9fdb86ae37d8427031009ca34b0793339977d9
Lex Buright <lex.buright@gmail.com>
2017-01-05 01:55:14 +0000

    qssettings -- convenience functions for storing settings
    Mostly logic to make it easy to interact with stuff from a
    gui. Silly wrapper around qstimeseries

---
+++ qscli/qssettings.py
@@ -0,0 +1,133 @@
+#!/usr/bin/python
+
+"""
+qssettings show # prompt for a setting name and print it
+qssettings set # prompt for a setting name and print it
+
+"""
+
+import argparse
+import json
+import logging
+import os.path
+import subprocess
+import sys
+
+from . import guiutils
+from .symbol import Symbol
+
+LOGGER = logging.getLogger()
+
+
+PROMPT = Symbol('prompt')
+
+def name_setting(parser):
+    parser.add_argument('--name', '-n', default=PROMPT)
+
+def value_setting(parser):
+    parser.add_argument('--value', '-v', default=PROMPT)
+
+DEFAULT_CONFIG_DIR = os.path.join(os.environ['HOME'], '.config', 'qssettings')
+
+PARSER = argparse.ArgumentParser(description='Store and update settings')
+PARSER.add_argument('--config-dir', '-C', help='Directory to store configuration and data', default=DEFAULT_CONFIG_DIR)
+PARSER.add_argument('--prefix', '-P', help='Prefix for settings in qstimeseries', default='')
+PARSER.add_argument('--debug', action='store_true', help='Print debug output')
+
+parsers = PARSER.add_subparsers(dest='action')
+show_parser = parsers.add_parser('show', help='')
+name_setting(show_parser)
+update_parser = parsers.add_parser('update', help='Update a setting')
+name_setting(update_parser)
+value_setting(update_parser)
+
+timeseries_parser = parsers.add_parser('timeseries', help='Update a setting')
+timeseries_parser.add_argument('arguments', nargs='*', help='Arguments to pass to timeseries')
+
+
+def main():
+    result = run(guiutils, sys.argv[1:])
+    if result is not None:
+        print result
+        sys.stdout.flush()
+
+def run(prompter, args):
+    args = PARSER.parse_args(args)
+    store = ValueStore(args.config_dir, args.prefix)
+
+    if args.debug:
+        logging.basicConfig(level=logging.DEBUG)
+
+    if args.action == 'show':
+        return show_setting(store, prompter, args.name)
+    elif args.action == 'update':
+        return update_setting(store, prompter, args.name, args.value)
+    elif args.action == 'timeseries':
+        return shell_collect(['qstimeseries', '--config-dir', args.config_dir] + args.arguments)
+    else:
+        raise ValueError(args.action)
+
+class ValueStore(object):
+    def __init__(self, config_dir, prefix):
+        self._config_dir = config_dir
+        self._prefix = prefix.strip('.') + '.'
+
+    def get_current(self, name):
+        data = json.loads(
+            shell_collect([
+                'qstimeseries',
+                '--config-dir',
+                self._config_dir,
+                'show',
+                '--series', self._prefix + name,
+                '--json']))
+        data.sort(key=lambda x: x['id'])
+        return data[-1]['value']
+
+    def get_setting_names(self):
+        result = json.loads(self.timeseries(['series', '--json']))
+        return [x['name'][len(self._prefix):]
+                    for x in result['series']
+                    if x['name'].startswith(self._prefix)]
+
+    def update(self, name, value):
+        if self._prefix:
+            name = self._prefix + name
+
+        LOGGER.debug('Storing %r %r', name, value)
+        return self.timeseries(['append', name, str(value)])
+
+    def timeseries(self, command):
+        return shell_collect(['qstimeseries', '--config-dir', self._config_dir] + command)
+
+def show_setting(store, prompter, name):
+    if name == PROMPT:
+        name = prompt_for_name(prompter, store)
+
+    return str(store.get_current(name))
+
+def prompt_for_name(prompter, store):
+    return prompter.combo_prompt('Which setting:', choices=store.get_setting_names())
+
+def prompt_for_value(prompter, store, default):
+    return prompter.float_prompt('Value:', default=default)
+
+def update_setting(store, prompter, name, value):
+    if name == PROMPT:
+        name = prompt_for_name(prompter, store)
+
+    if value == PROMPT:
+        old = store.get_current(name)
+        value = prompt_for_value(prompter, store, default=old)
+
+    store.update(name, value)
+
+def shell_collect(command):
+	process = subprocess.Popen(command, stdout=subprocess.PIPE)
+	stdout, _ = process.communicate()
+	if process.returncode != 0:
+		raise ValueError(process.returncode)
+	return stdout
+
+if __name__ == '__main__':
+    main()
---
+++ tests/test_settings.py
@@ -0,0 +1,56 @@
+import shutil
+import tempfile
+import unittest
+
+from qscli.qssettings import run
+
+class TestQssettings(unittest.TestCase):
+    def setUp(self):
+        self.direc = tempfile.mkdtemp()
+        self.prompter = FakePrompter()
+
+    def tearDown(self):
+        shutil.rmtree(self.direc)
+
+    def run_cli(self, *args):
+        new_args = ('--config-dir', self.direc) + tuple(args)
+        return run(self.prompter, new_args)
+
+    def test_prefix(self):
+        self.run_cli('--prefix', 'prefix', 'update', '--name', 'test', '--value', '1.1')
+        self.run_cli('update', '--name', 'test', '--value', '2.0')
+        self.assertEquals(self.run_cli('show', '--name', 'test'), '2.0')
+        self.assertEquals(self.run_cli('--prefix', 'prefix', 'show', '--name', 'test'), '1.1')
+
+    def test_basic(self):
+        self.run_cli('update', '--name', 'test', '--value', '1.1')
+        self.assertEquals(self.run_cli('show', '--name', 'test'), '1.1')
+        self.run_cli('update', '--name', 'test', '--value', '1.2')
+        self.assertEquals(self.run_cli('show', '--name', 'test'), '1.2')
+
+    def test_prompter(self):
+        self.prompter.set_value('Which setting:', 'setting1')
+        self.run_cli('update', '--value', '1.1')
+        self.prompter.set_value('Which setting:', 'setting1')
+        self.assertEquals(self.run_cli('show'), '1.1')
+        self.prompter.set_value('Value:', 2.0)
+        self.run_cli('update')
+        self.assertEquals(self.run_cli('show'), '2.0')
+
+class FakePrompter(object):
+    def __init__(self):
+        self._values = {}
+
+    def set_value(self, prompt, value):
+        self._values[prompt] = value
+
+    def float_prompt(self, prompt, default=None):
+        del default
+        return self._values[prompt]
+
+    def combo_prompt(self, prompt, choices):
+        del choices
+        return self._values[prompt]
+
+if __name__ == '__main__':
+    unittest.main()
